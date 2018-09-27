e20ef517249bd3261601764c3352a7acba4dbd7f
LilitVahradyan <lilit95v@gmail.com>
2018-09-28 00:17:39 +0400

    second commit

--- read.php
+++ read.php
@@ -1,15 +1,20 @@
 <?php
 require_once 'db.php';
+
 $sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
+$users = array("id: " . $row["id"]. " - Username: " . $row["username"]. " " . $row["firstname"].  " " .$row["lastname"]. " "
+             .$row["email"]. " " .$row["password"]. " " .$row["birthday"]. " " .$row["gender"] ."<br>");
+
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        $users = array("id: " . $row["id"]. " - Username: " . $row["username"]. " " . $row["firstname"].  " " .$row["lastname"]. " "
-             .$row["email"]. " " .$row["password"]. " " .$row["birthday"]. " " .$row["gender"] ."<br>");
+        array_push($users, $row);
     }
-} else {
+}else {
     echo "0 results";
 }
+
+
 $conn->close();
 ?>
--- users.php
+++ users.php
@@ -3,3 +3,31 @@ require_once 'read.php';
 print_r($users);
 
 ?>
+<table border="1">
+
+    <tr>
+        <?php
+        foreach ($users[0] as $key=>$row){
+        ?>
+        <th>
+            <?= $key; ?>
+        </th>
+        <?php }?>
+    </tr>
+
+    <?php
+    foreach ($users as $user){
+
+    ?>
+    <tr>
+        <?php
+        foreach ($user as $us){
+
+            ?>
+            <td>
+                <?= $us; ?>
+            </td>
+        <?php }?>
+    </tr>
+    <?php }?>
+</table>
