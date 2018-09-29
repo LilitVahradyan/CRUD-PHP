67e1cab8569c110a470ca317ffe19d60a35cbece
LilitVahradyan <lilit95v@gmail.com>
2018-09-30 00:32:18 +0400

    php commit

--- update.php
+++ update.php
@@ -1,6 +1,8 @@
     <?php
 require_once 'db.php';
-    $user = "SELECT * FROM users WHERE  id=$_GET['editid']";
+    $id = $_GET['editid'];
+    $user = "SELECT * FROM users WHERE  id=$id";
+    
     $result = $conn->query($user);
     $data = $result->fetch_assoc();
    
@@ -10,15 +12,16 @@ require_once 'db.php';
 
 
 
-      <form action="" method=""> 
-       <input type="text" name="username" value="<?php $user.username?>" placeholder="Username"><br><br>
-	   <input type="text" name="firstname" value="<?php $user.firstname?>" placeholder="First Name"><br><br>
-	   <input type="text" name="lastname" value="<?php $user.lastname?>" placeholder="LastName"><br><br>
-	   <input type="email" name="email" value="<?php $user.email?>" placeholder="Email"><br><br>
-	   <input type="password" name="password" value="<?php $user.password?>" placeholder="password"><br><br>
-	   <input type="date" name="birthday" value="<?php $user.birthday?>" placeholder="Birthday"><br><br>
-	    <input type="radio" name="gender" value="<?php $user.male?>"> Male<br>
-       <input type="radio" name="gender" value="<?php $user.female?>"> Female<br>
+      <form action="updateUser.php" method="post"> 
+      	<input type="hidden" name="editId" value="<?= $_GET['editId']?>">
+       <input type="text" name="username" value="<?= $data.username?>" placeholder="Username"><br><br>
+	   <input type="text" name="firstname" value="<?= $data.firstname?>" placeholder="First Name"><br><br>
+	   <input type="text" name="lastname" value="<?= $data.lastname?>" placeholder="LastName"><br><br>
+	   <input type="email" name="email" value="<?= $data.email?>" placeholder="Email"><br><br>
+	   <input type="password" name="password" value="<?= $data.password?>" placeholder="password"><br><br>
+	   <input type="date" name="birthday" value="<?= $data.birthday?>" placeholder="Birthday"><br><br>
+	    <input type="radio" name="gender" value="<?= $data.male?>"> Male<br>
+       <input type="radio" name="gender" value="<?= $data.female?>"> Female<br>
 	 
 	   <input type="submit" value="Send">
 	</form>
